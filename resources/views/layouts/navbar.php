d0b40dd8ca93a1290b667594090036e232a4cd03
MuhammadHafidzrr <apitr5960@gmail.com>
2023-11-28 08:38:22 +0700

    new

--- resources/views/layouts/navbar.php
+++ resources/views/layouts/navbar.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="bs/css/bootstrap.min.css">
-</head>
+ </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
@@ -32,7 +32,7 @@
         </ul>
     </div>
     <form class="d-flex" role="Login">
-      <a class="btn btn-danger " aria-current="page" href="/logout">Logout</a>
+      <a class="btn btn-danger " aria-current="page" href="/logout">Logout</aa 
        <div class="container">
        <a class="btn btn-primary " aria-current="page" href="register">Daftar</a> 
       </form>
